76339d1a7bca47168691d4f3018af275aa73a295
qiaoqiao2323 <92834285+qiaoqiao2323@users.noreply.github.com>
2025-01-19 17:00:05 +0000

    Deploying to gh-pages from @ qiaoqiao2323/qiaoqiao2323.github.io@8dc9d3bc34fc8cd20d9d6187ea4c31bf11017c9f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,7 +65,7 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-wins-the-best-paper-award-at-the-international-conference-on-social-robotics-2024",
           title: 'Our paper wins the best paper award at the International Conference on Social...',
           description: "",
-          section: "News",},{id: "news-our-paper-touched-by-chatgpt-using-an-llm-to-drive-affective-tactile-interaction-was-accepted-as-a-late-breaking-report-at-acm-ieee-hri-2025-in-paris",
+          section: "News",},{id: "news-our-paper-touched-by-chatgpt-using-an-llm-to-drive-affective-tactile-interaction-was-accepted-as-a-late-breaking-report-at-acm-ieee-hri-2025",
           title: 'Our paper “Touched by ChatGPT: Using an LLM to Drive Affective Tactile Interaction”,...',
           description: "",
           section: "News",},{id: "projects-tactile-interaction-influences-risk-taking-behaviour-and-attitude",
