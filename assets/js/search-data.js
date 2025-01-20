3d201ee8cf5052de0c2d70c6ae2a90b40671d02f
qiaoqiao2323 <92834285+qiaoqiao2323@users.noreply.github.com>
2025-01-20 17:26:29 +0000

    Deploying to gh-pages from @ qiaoqiao2323/qiaoqiao2323.github.io@6ac34bc2318136f83a375bdab288ecea37bab082 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -83,7 +83,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-touched-by-chatgpt-using-an-llm-to-drive-affective-tactile-interaction",
           title: 'Touched by ChatGPT; Using an LLM to Drive Affective Tactile Interaction',
-          description: "a project with a background image",
+          description: "Qiaoqiao Ren and Tony Belpaeme",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{
