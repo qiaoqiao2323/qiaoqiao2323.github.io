ff970fd502fb032a736251761fb4d1b83715de70
qiaoqiao2323 <92834285+qiaoqiao2323@users.noreply.github.com>
2025-01-20 13:14:18 +0000

    Deploying to gh-pages from @ qiaoqiao2323/qiaoqiao2323.github.io@b5912017e5a98a908673d28793d1bcb17def180e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -66,9 +66,9 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
