904b08440928e83a4f8f03c5a901a1e9913c03b8
qiaoqiao2323 <92834285+qiaoqiao2323@users.noreply.github.com>
2025-01-20 13:17:22 +0000

    Deploying to gh-pages from @ qiaoqiao2323/qiaoqiao2323.github.io@208b82d5fe70c440cf0c44ca64e3b8e90a312173 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -76,8 +76,8 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
+            },},{id: "projects-multimodal-superiority-vibrotactile-and-sound-in-robot-human-interaction",
+          title: 'Multimodal Superiority; Vibrotactile and Sound in Robot-Human Interaction',
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
