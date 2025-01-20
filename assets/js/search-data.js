c843c0a26f297132e45ebbb02772a16b5d1fc09e
qiaoqiao2323 <92834285+qiaoqiao2323@users.noreply.github.com>
2025-01-20 13:16:08 +0000

    Deploying to gh-pages from @ qiaoqiao2323/qiaoqiao2323.github.io@550f9bbda3184126820e96101a6d0ee0a9d553a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -71,8 +71,8 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-conveying-emotions-to-robots-through-touch-and-sound",
+          title: 'Conveying Emotions to Robots through Touch and Sound',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
